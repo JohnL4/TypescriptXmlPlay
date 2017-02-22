0ffcf2792660a29f374d3479532bb9c9be9ed813
John Lusk <john-github@how-hard-can-it-be.com>
2017-02-21 22:41:15 -0500

    More complex XML to parse, plus a schema definition, just for completeness.

--- xml-play.ts
+++ xml-play.ts
@@ -2,17 +2,41 @@ import { StarSystem } from "./StarSystem";
 
 console.clear();
 
+// var clusterXml = `<?xml version="1.0"?>
+// <cluster xmlns="http://www.how-hard-can-it-be.com/diaspora/cluster"
+//          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+//          xsi:schemaLocation="http://www.how-hard-can-it-be.com/diaspora/cluster/cluster.xsd">
+//   <starSystem id="Alpha" technology="-1" environment="4" resources="-1"/>
+//   <starSystem id="Bravo" technology="2" environment="1" resources="1"/>
+//   <starSystem id="Charlie" technology="-1" environment="1" resources="-2"/>
+//   <starSystem id="Delta" technology="1" environment="1" resources="1"/>
+//   <starSystem id="Echo" technology="0" environment="-3" resources="1"/>
+//   <starSystem id="Foxtrot" technology="0" environment="-1" resources="3"/>
+// </cluster>`
+
 var clusterXml = `<?xml version="1.0"?>
-<cluster xmlns="http://www.how-hard-can-it-be.com/diaspora/cluster"
+<cluster xmlns="http://how-hard-can-it-be.com/diaspora"
          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://www.how-hard-can-it-be.com/diaspora/cluster/cluster.xsd">
-  <starSystem id="Alpha" technology="-1" environment="4" resources="-1"/>
-  <starSystem id="Bravo" technology="2" environment="1" resources="1"/>
-  <starSystem id="Charlie" technology="-1" environment="1" resources="-2"/>
-  <starSystem id="Delta" technology="1" environment="1" resources="1"/>
-  <starSystem id="Echo" technology="0" environment="-3" resources="1"/>
-  <starSystem id="Foxtrot" technology="0" environment="-1" resources="3"/>
-</cluster>`
+         xsi:schemaLocation="http://how-hard-can-it-be.com/diaspora cluster.xsd">
+  <starSystem id="Alpha" technology="0" environment="2" resources="-1">
+    <slipstream to="Bravo" leave="high" arrive="low"/> <!-- to: idref (limitation: max one occurrence of destination per system); leave/arrive: enumerations -->
+    <slipstream to="Charlie" leave="low" arrive="low"/>
+    <aspect>Gentlemen, I give you the Pencarrow Endeavor!</aspect>
+    <aspect>You're not from around here, are you?</aspect>
+    <notes>
+      Arbitrary text, multi-line, multi-paragraph, possibly with some limited HTML markup, or possibly some markdown.
+    </notes>
+  </starSystem>
+  <starSystem id="Bravo" technology="-1" environment="-3" resources="-1">
+    <slipstream to="Charlie" leave="low" arrive="low"/>
+  </starSystem>
+  <starSystem id="Charlie" technology="2" environment="-1" resources="0">
+    <slipstream to="Charlie" leave="low" arrive="low"/>
+  </starSystem>
+  <starSystem id="Delta" technology="2" environment="-1" resources="-1">
+    <slipstream to="Charlie" leave="low" arrive="low"/>
+  </starSystem>
+</cluster>`;
 
 console.log(clusterXml);
 
